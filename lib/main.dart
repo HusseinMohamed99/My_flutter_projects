2e626bfa07f3c588f679504ec45f4273ce65aa9f
Hussein Mohamed <84459939+husseinM1599@users.noreply.github.com>
2022-03-27 21:01:03 +0200

    bmi_calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_projects/bloc_observer.dart';
-import 'package:flutter_projects/layout/todo_app/todo_layout.dart';
-import 'package:flutter_projects/network/local/cache_helper.dart';
-import 'package:flutter_projects/network/remote/dio_helper.dart';
-import 'package:flutter_projects/shared/cubit/cubit.dart';
-import 'package:flutter_projects/styles/themes.dart';
+import 'package:flutter_projects/modules/BMI_Screen.dart';
 
-
-import 'shared/cubit/states.dart';
-
-void main() async {
-
-  // بيتأكد ان كل حاجة خلصت و بعدين يرن
-  WidgetsFlutterBinding.ensureInitialized();
-
-  Bloc.observer = MyBlocObserver();
-  DioHelper.init();
-  await CacheHelper.init();
-
-  bool isDark = CacheHelper.getData(key: 'isDark');
-
-
-
-  runApp(MyApp(
-    isDark: isDark,
-
-  ));
+void main() {
+  runApp(MyApp());
 }
+
 // Stateless
 // Stateful
+
 // class MyApp
+
 class MyApp extends StatelessWidget {
+// constructor
+// build
 
-  final bool isDark;
-
-
-  MyApp({
-    this.isDark,
-
-  });
-  // constructor
-  // build
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-
-        BlocProvider(create:(context)=> AppCubit()
-          ..changeAppMode(
-            fromShared: isDark,
-          )
-        ),
-
-
-      ],
-      child: BlocConsumer<AppCubit,AppStates>(
-        listener: (context,state){},
-        builder: (context,state)
-        {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: lightTheme,
-            darkTheme: darkTheme,
-            themeMode:
-            AppCubit.get(context).isDark? ThemeMode.light : ThemeMode.dark,
-            home:Home_Layout(),
-          );
-        },
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:BMI_Screen(),
     );
   }
 }
---
+++ lib/modules/BMI_Result_Screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class BMI_Result_Screen extends StatelessWidget {
+
+ final int Result;
+ final bool isMale;
+ final int Age;
+
+ BMI_Result_Screen({
+   @required this.Result,
+   @required this.Age,
+   @required this.isMale,
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: HexColor('#04043A'),
+      appBar: AppBar(
+        backgroundColor:  HexColor('#800080') ,
+        title: Text(
+          'BMI Result',
+          style: TextStyle(
+            fontSize: 25.0,
+
+          ),
+        ),
+
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Container(
+            width: double.infinity,
+            height: 300.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0,),
+              color:  HexColor('#800080') ,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+
+                    Text(
+                      'Result : $Result ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 1.0,
+                    ),
+                    Text(
+                      'BMI ',
+
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                  ],
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(10.0,),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(2.0,),
+                      color: Colors.blue,
+
+                    ),
+                    height: 5.0,
+                    width: double.infinity,
+
+                  ),
+                ),
+
+                SizedBox(
+                  height: 5.0,
+                ),
+
+                Text(
+                  'Gender : ${isMale ? 'Male' : 'Female'} ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                SizedBox(
+                  height: 5.0,
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(10.0,),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(2.0,),
+                      color: Colors.blue,
+
+                    ),
+                    height: 5.0,
+                    width: double.infinity,
+
+                  ),
+                ),
+
+                Text(
+                  'Age : $Age ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+
+    );
+  }
+}
