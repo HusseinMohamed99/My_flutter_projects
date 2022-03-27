8c5568715b2baf839dc19b427ba774a7e1f9b56f
Hussein Mohamed <84459939+husseinM1599@users.noreply.github.com>
2022-03-27 20:52:00 +0200

    to-do-App

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_projects/List_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_projects/bloc_observer.dart';
+import 'package:flutter_projects/layout/todo_app/todo_layout.dart';
+import 'package:flutter_projects/network/local/cache_helper.dart';
+import 'package:flutter_projects/network/remote/dio_helper.dart';
+import 'package:flutter_projects/shared/cubit/cubit.dart';
+import 'package:flutter_projects/styles/themes.dart';
 
 
-void main() {
-  runApp( MyApp());
+import 'shared/cubit/states.dart';
+
+void main() async {
+
+  // بيتأكد ان كل حاجة خلصت و بعدين يرن
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Bloc.observer = MyBlocObserver();
+  DioHelper.init();
+  await CacheHelper.init();
+
+  bool isDark = CacheHelper.getData(key: 'isDark');
+
+
+
+  runApp(MyApp(
+    isDark: isDark,
+
+  ));
 }
-
+// Stateless
+// Stateful
+// class MyApp
 class MyApp extends StatelessWidget {
 
-  // This widget is the root of your application.
+  final bool isDark;
+
+
+  MyApp({
+    this.isDark,
+
+  });
+  // constructor
+  // build
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: List_Screen(),
+    return MultiBlocProvider(
+      providers: [
+
+        BlocProvider(create:(context)=> AppCubit()
+          ..changeAppMode(
+            fromShared: isDark,
+          )
+        ),
+
+
+      ],
+      child: BlocConsumer<AppCubit,AppStates>(
+        listener: (context,state){},
+        builder: (context,state)
+        {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            themeMode:
+            AppCubit.get(context).isDark? ThemeMode.light : ThemeMode.dark,
+            home:Home_Layout(),
+          );
+        },
+      ),
     );
   }
 }
-
---
+++ lib/modules/todo_app/new_Done/New_Done_Screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_projects/shared/components/componentes.dart';
+import 'package:flutter_projects/shared/cubit/cubit.dart';
+import 'package:flutter_projects/shared/cubit/states.dart';
+
+
+class New_Done_Screen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AppCubit,AppStates>(
+      listener:(context, state) {},
+      builder: (context,state){
+
+        var tasks = AppCubit.get(context).DoneTasks;
+
+        return tasksBuilder (
+          tasks: tasks,
+        );
+      },
+
+    );
+  }
+}
+
+
+
