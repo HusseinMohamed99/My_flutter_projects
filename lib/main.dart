d995ab69ecb9f2bd553de3c6be276d6e598be2cb
Hussein Mohamed <84459939+husseinM1599@users.noreply.github.com>
2022-03-27 20:05:48 +0200

    counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_projects/messenger_screen.dart';
+import 'package:flutter_projects/Counter_Screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-// Stateless
-// Stateful
-
-// class MyApp
-
 class MyApp extends StatelessWidget {
-// constructor
-// build
 
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: messenger_screen(),
+
+      home:Counter_Screen() ,
     );
   }
 }
