4d308d4f5b5a4a0c69a706bf25281fb23467ae1c
Hussein Mohamed <84459939+husseinM1599@users.noreply.github.com>
2022-04-12 01:51:56 +0200

    Master

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_projects/login/login_screen.dart';
+import 'package:flutter_projects/HomeScreen.dart';
 
 
 
-void main()async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+void main() {
+
   runApp(MyApp());
 }
 
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: HomeScreen(),
     );
   }
 }
