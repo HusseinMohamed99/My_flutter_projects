059d6efbb5211e145edfad0acf9635e1858521b2
Hussein Mohamed <84459939+husseinM1599@users.noreply.github.com>
2022-05-10 23:48:53 +0200

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,157 @@
+import 'dart:convert';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Container(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, @required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Future<bool> callFcmApiSendPushNotification({
+    @required String title,
+    @required String body,
+  }) async {
+    const postUrl = 'https://fcm.googleapis.com/fcm/send';
+    final data = {
+      "to": "/topics/myTopic",
+      "notification": {
+        "title": title,
+        "body": body,
+        "sound": "default",
+      },
+      "data": {
+        "type": 'Order',
+        "id": '20',
+        "click_action": 'FLUTTER_NOTIFICATION_CLICK',
+      }
+    };
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization':
+          'key=AAAANi0ZKzw:APA91bEsHAqpdXsPO7eDcpcyeAQARpIoBWz5732BgtLabTC3bs4Qp5KZMB7woIfB2WYDXT7v2zIAuQv4WYZyfkhRVFzJ8zNSVbhkGAihifrB8WqgjbWivMOjdZ9ba0XJOlYeno6Urp7G'
+    };
+    final response = await http.post(Uri.parse(postUrl),
+        body: json.encode(data),
+        encoding: Encoding.getByName('utf-8'),
+        headers: headers);
+    if (response.statusCode == 200) {
+      print('test ok push CFM');
+      return true;
+    } else {
+      print(' CFM error');
+      return false;
+    }
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      callFcmApiSendPushNotification(title: 'fcm by api', body: 'its working');
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseMessaging.instance.subscribeToTopic('myTopic');
+              },
+              child: Text('Send Notification'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseMessaging.instance
+                    .unsubscribeFromTopic('connectTopic');
+              },
+              child: Text('Send Notification'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
